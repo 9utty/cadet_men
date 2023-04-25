0b4f246a55cacb43c090f92561a1abc9af6c5934
9utty <leegh4250@gmail.com>
2023-04-25 20:40:30 +0900

    feat: 메뉴바 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,6 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {" "}
-      <div>42Manito</div>
       <Component {...pageProps} />
     </>
   );
--- pages/global/AppLayout.tsx
+++ pages/global/AppLayout.tsx
@@ -1,14 +1,24 @@
 import Link from "next/link";
+import { Menu } from "antd";
 
 export default function AppLayout() {
   return (
     <div>
-      <div>
-        <Link href="/home/Home">홈</Link>
-        <Link href="/mento/Mento">멘토</Link>
-        <Link href="/category/Category">카테고리</Link>
-        <Link href="/search/Search">검색</Link>
-      </div>
+      <div>42Manito</div>
+      <Menu mode="horizontal" theme="dark">
+        <Menu.Item>
+          <Link href="/home/Home">홈</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/mento/Mento">멘토</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/category/Category">카테고리</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/search/Search">검색</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   );
 }
