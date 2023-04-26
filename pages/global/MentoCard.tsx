cac53a706a3545a1fa3e0ebcf1f8536d86b5b540
9utty <leegh4250@gmail.com>
2023-04-26 23:36:28 +0900

    feat: 리덕스 셋팅

---
+++ pages/global/MentoCard.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Card, Grid } from "antd";
+
+const { Meta } = Card;
+const { useBreakpoint } = Grid;
+
+const MentoCard = () => {
+  const screens = useBreakpoint();
+  const cardWidth = screens.xs ? 140 : 240;
+  console.log(cardWidth);
+
+  return (
+    <Card
+      hoverable
+      style={{ width: cardWidth }}
+      cover={
+        <img
+          alt="example"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+        />
+      }
+    >
+      <Meta title="gulee" />
+      <Meta description="저는 보컬입니다" />
+      <Meta description="#보컬" />
+      <Meta description="#밴드" />
+    </Card>
+  );
+};
+
+export default MentoCard;
