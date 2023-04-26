21f873689131a769cbd81d95747a145a6c1c8683
9utty <leegh4250@gmail.com>
2023-04-27 08:58:09 +0900

    feat: Month 랭킹 생성

---
+++ pages/ranking/components/Month.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Divider, Row, Col } from "antd";
+import MentorCard from "@/pages/global/MentorCard";
+
+const MonthlyRanking = () => {
+  return (
+    <>
+      <Row>
+        <Col></Col>
+      </Row>
+    </>
+  );
+};
+
+export default MonthlyRanking;
