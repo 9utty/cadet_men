3889331e4e546824f51a635628f9d4e84c1bb22a
9utty <leegh4250@gmail.com>
2023-04-27 04:00:46 +0900

    feat: feedBack page 생성

---
+++ pages/feedback/feedback.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppLayout from "../global/AppLayout";
+
+const Feedback = () => {
+  return (
+    <AppLayout>
+      <div>피드백페이지입니다.</div>
+    </AppLayout>
+  );
+};
+
+export default Feedback;
