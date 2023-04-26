071c9685e298e410032f22731860fdb5b0e89c37
9utty <leegh4250@gmail.com>
2023-04-27 08:59:53 +0900

    feat: Total Ranking 페이지 생성

---
+++ pages/ranking/components/Total.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Divider, Row, Col } from "antd";
+
+const TotalRanking = () => {
+  return (
+    <>
+      <Row>
+        <Col></Col>
+      </Row>
+    </>
+  );
+};
+
+export default TotalRanking;
