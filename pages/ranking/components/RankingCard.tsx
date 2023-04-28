6972f309b3c3e43e0eb41e8085bbe6a49eeb4bad
9utty <leegh4250@gmail.com>
2023-04-28 16:55:50 +0900

    feat: RankingCard가 순위 받아서 출력하게 설정

--- pages/ranking/components/RankingCard.tsx
+++ pages/ranking/components/RankingCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, Grid, Divider, Image } from "antd";
 import Link from "next/link";
 import MentorProfile from "@/pages/profiles/MentorProfile";
+import { FaCrown } from "react-icons/fa";
 
 const { Meta } = Card;
 const { useBreakpoint } = Grid;
@@ -10,48 +11,76 @@ interface Card {
   image: string;
   userNickName: string;
   count: number;
+  rank: number;
 }
 
 const RankingCard = (props: Card) => {
   const screens = useBreakpoint();
   const cardWidth = screens.xs ? 120 : 240;
   const maxDescriptionLength = screens.xs ? 32 : 40;
+
   return (
     <Link href="../../../profiles/MentorProfile">
-      <Card
-        hoverable
-        style={{ width: cardWidth }}
-        cover={
-          <Image
-            preview={false}
-            alt="example"
-            src={props.image}
-            height={screens.xs ? 120 : 240}
-            width={screens.xs ? 120 : 240}
-            style={{ objectFit: "cover", borderRadius: "50%" }}
-          />
-        }
-      >
-        <div
-          style={{
-            fontSize: screens.xs ? "15px" : "25px",
-            color: screens.xs ? "#000" : "#000",
-            fontWeight: "bold",
-            marginBottom: "8px",
-          }}
+      <div>
+        {" "}
+        <Card
+          hoverable
+          style={{ width: cardWidth }}
+          cover={
+            <div>
+              <Image
+                preview={false}
+                alt="example"
+                src={props.image}
+                height={screens.xs ? 110 : 220}
+                width={screens.xs ? 110 : 220}
+                style={{
+                  objectFit: "cover",
+                  borderRadius: "50%",
+                  marginLeft: screens.xs ? "5px" : "10px",
+                  marginTop: screens.xs ? "5px" : "10px",
+                }}
+              />
+              {props.rank <= 3 ? (
+                <FaCrown
+                  style={{
+                    color:
+                      props.rank === 1
+                        ? "gold"
+                        : props.rank === 2
+                        ? "silver"
+                        : "brown",
+                    fontSize: "30px",
+                    position: "absolute",ㄱ
+                    top: 0,
+                    right: 0,
+                  }}
+                />
+              ) : null}
+            </div>
+          }
         >
-          {props.userNickName}
-        </div>
-        <div
-          style={{
-            fontSize: screens.xs ? "13px" : "22px",
-            color: screens.xs ? "#000" : "#000",
-            marginBottom: "8px",
-          }}
-        >
-          {`멘토링 ${props.count}회`}
-        </div>
-      </Card>
+          <div
+            style={{
+              fontSize: screens.xs ? "15px" : "25px",
+              color: screens.xs ? "#000" : "#000",
+              fontWeight: "bold",
+              marginBottom: "8px",
+            }}
+          >
+            {props.userNickName}
+          </div>
+          <div
+            style={{
+              fontSize: screens.xs ? "11px" : "22px",
+              color: screens.xs ? "#000" : "#000",
+              marginBottom: "8px",
+            }}
+          >
+            {`멘토링 ${props.count}`}
+          </div>
+        </Card>
+      </div>
     </Link>
   );
 };
