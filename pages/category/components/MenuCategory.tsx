951c83a2fbe34e8a71269d01ea6dcd708a20e570
9utty <leegh4250@gmail.com>
2023-04-29 16:19:01 +0900

    refactor: 카테고리 메뉴이름 및 위치 수정

--- pages/category/components/MenuCategory.tsx
+++ pages/category/components/MenuCategory.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import { Menu, Divider } from "antd";
+import { Menu, Divider, Row, Col } from "antd";
 import Category from "../Category";
 import StudyCategory from "./StudyCategory";
 import HobbyCategory from "./HobbyCategory";
@@ -15,35 +15,33 @@ const MenuCategory = () => {
   };
   return (
     <>
-      <Menu
-        mode="horizontal"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Menu.Item key="study" onClick={onStudy}>
+      <Menu mode="horizontal">
+        <Menu.Item key="study" onClick={onStudy} style={{ flex: 1 }}>
           <div
             style={{
-              fontSize: "25px",
-              marginLeft: "10px",
-              marginRight: "10px",
+              fontSize: "22px",
+
+              color: "#555",
+              justifyContent: "center",
+              alignItems: "center",
+              display: "flex",
             }}
           >
-            공부
+            Coding
           </div>
         </Menu.Item>
-        <Divider type="vertical" />
-        <Menu.Item key="hobby" onClick={onHobby}>
+        <Menu.Item key="hobby" onClick={onHobby} style={{ flex: 1 }}>
           <div
             style={{
-              fontSize: "25px",
-              marginLeft: "10px",
-              marginRight: "10px",
+              fontSize: "22px",
+
+              color: "#555",
+              justifyContent: "center",
+              alignItems: "center",
+              display: "flex",
             }}
           >
-            취미
+            Hobby
           </div>
         </Menu.Item>
       </Menu>
