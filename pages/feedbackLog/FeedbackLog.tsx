5b855a4ec4a66aca4ab611f27eec6480da29659d
9utty <leegh4250@gmail.com>
2023-04-30 17:20:17 +0900

    feat: 데이터 수정해야함

--- pages/feedbackLog/FeedbackLog.tsx
+++ pages/feedbackLog/FeedbackLog.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import AppLayout from "../global/AppLayout";
+import Ap
+pLayout from "../global/AppLayout";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const FeedbackLog = () => {
   return (
     <AppLayout>
-      <InfiniteScroll />
+      {/* <InfiniteScroll /> */}
       <div>피드백로그페이지입니다.</div>
     </AppLayout>
   );
