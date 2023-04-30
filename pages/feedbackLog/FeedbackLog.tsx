e700bca2e0e2a1510a6135f15b71bbc43c074fa6
9utty <leegh4250@gmail.com>
2023-04-30 17:17:50 +0900

    feat: Feedback log page 무한스크롤로 수정

--- pages/feedbackLog/FeedbackLog.tsx
+++ pages/feedbackLog/FeedbackLog.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import AppLayout from "../global/AppLayout";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const FeedbackLog = () => {
   return (
     <AppLayout>
+      <InfiniteScroll />
       <div>피드백로그페이지입니다.</div>
     </AppLayout>
   );
