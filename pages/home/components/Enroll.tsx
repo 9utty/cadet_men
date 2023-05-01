943bc8ab3c4aa29ae4cc31f51d0a770cbfda34a8
9utty <leegh4250@gmail.com>
2023-05-01 22:21:36 +0900

    feat: Home에 엔롤뷰 구성

---
+++ pages/home/components/Enroll.tsx
@@ -0,0 +1,12 @@
+import AppLayout from "@/pages/global/AppLayout";
+import React from "react";
+
+const HomeEnroll = () => {
+  return (
+    <AppLayout>
+      <div>홈에서 엔롤입니다(멘토, 멘티 엔롤리스트)</div>
+    </AppLayout>
+  );
+};
+
+export default HomeEnroll;
