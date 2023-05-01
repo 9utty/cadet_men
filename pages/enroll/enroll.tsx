5f9838c27252fcf24278589e078213bcde0bddef
9utty <leegh4250@gmail.com>
2023-05-01 13:49:26 +0900

    feat: 멘토링 멘티 참여리스트 페이지 구성

---
+++ pages/enroll/enroll.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppLayout from "../global/AppLayout";
+
+const Enroll = () => {
+  return (
+    <AppLayout>
+      <div>엔롤페이지입니다.</div>
+    </AppLayout>
+  );
+};
+
+export default Enroll;
